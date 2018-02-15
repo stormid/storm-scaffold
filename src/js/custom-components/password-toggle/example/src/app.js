f58070deccad6ba0024f887a343b59848e1a0cb7
Mick Perkins <mick.perkins@stormid.com>
2018-02-15 14:56:39 +0000

    Removes test custom-component, adds static dir, fixes #1

--- src/js/custom-components/password-toggle/example/src/app.js
+++
@@ -1,7 +0,0 @@
-import PasswordToggle from './libs/storm-password-toggle';
-
-const onDOMContentLoadedTasks = [() => {
-    PasswordToggle.init('.js-password__toggle');
-}];
-    
-if('addEventListener' in window) window.addEventListener('DOMContentLoaded', () => { onDOMContentLoadedTasks.forEach((fn) => fn()); });
