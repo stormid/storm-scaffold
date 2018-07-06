45908b467526fca1a7bc6e559d02b95c4c64967f
mjbp <mick.perkins@stormid.com>
2018-07-06 15:21:57 +0100

    Adds macros re #6, removes scss defaults re #4

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,8 @@
-// import { PATHS } from './constants';
+import { PATHS } from './constants';
 import Promise from 'promise-polyfill';
-// import 'storm-outliner';
-//import 'lazysizes' from 'lazysizes';
-// import Toggle from './require/toggle';
-// import AsyncHelper from './require/async-helper';
-// import FontFaceObserver from './require/fontfaceobserver';
 import Load from 'storm-load';
 
-const onInit = [
-	// Toggle,
-	// AsyncHelper('tabs'),
-	// AsyncComponentHelper('google-map').bind(null, () => {
-	// 	GoogleMap.init(GOOGLE_MAP.SELECTOR, window.event_location, {
-	// 		key: GOOGLE_MAP.KEY
-	// 	});
-	// }),
-	// FontFaceObserver,
-];
+const onInit = [];
 
 const onLoad = [];
 
@@ -25,12 +11,10 @@ const onLoad = [];
 
 	if(!Object.assign || !('classList' in document.createElement('_'))) 
 		Load(`${PATHS.JS_ASYNC}/polyfills.min.js`)
-			.then(() => { 
-				onInit.forEach(fn => fn());
-			});
-	else onInit.forEach(fn => fn());
+			.then(() => onInit.map(f => f()));
+	else onInit.map(fn => fn());
 
-	onLoad.length && window.addEventListener('load', [].forEach.bind(onLoad, fn => fn()));
+	onLoad.length && window.addEventListener('load', [].map.bind(onLoad, f => f()));
 
 	//if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js');
 }
