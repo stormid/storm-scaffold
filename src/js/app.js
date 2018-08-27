fb863781066e61f378b393ac5c2c7b7c97c20529
Mick Perkins <mick.perkins@stormid.com>
2018-08-27 13:40:20 +0100

    Changes when no-js className is removed

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,13 @@ import { PATHS } from './constants';
 import Promise from 'promise-polyfill';
 import Load from 'storm-load';
 
-const onInit = [
-	() => { document.documentElement.className =  document.documentElement.className.split('no-js').join(''); }
-];
+const onInit = [];
 
 const onLoad = [];
 
 {
 	window.Promise = window.Promise ? window.Promise : Promise;
+	document.documentElement.className =  document.documentElement.className.split('no-js').join('');
 
 	if(!Object.assign) {
 		Load(`${PATHS.JS_ASYNC}/polyfills.min.js`)
