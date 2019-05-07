4e8130c63f4a2eff5815115c2c61b4da5b79320a
Mick Perkins <mickperkins@gmail.com>
2019-05-07 21:11:11 +0100

    Minor renaming and clean up

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,12 @@
 import { PATHS } from './constants';
 import Promise from 'promise-polyfill';
 import Load from 'storm-load';
-const r = f => f();
-const onInit = [];
+const initStack = [];
 
 {
     window.Promise = window.Promise ? window.Promise : Promise;
     document.documentElement.className = document.documentElement.className.split('no-js').join('');
 
-    if (!Object.assign) {
-        Load(`${PATHS.JS_ASYNC}/polyfills.min.js`).then(() => onInit.map(r));
-    } else onInit.map(r);
+    if (!Object.assign) Load(`${PATHS.JS_ASYNC}/polyfills.min.js`).then(() => initStack.map(fn => fn()));
+    else initStack.map(fn => fn());
 }
